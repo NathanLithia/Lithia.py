3627ebc385de9a769556e111bde3bd09c2effc2f
Nathan - Lithia <nathanlol5@live.com.au>
2022-01-07 06:49:53 +1030

    Webhook/Bot Replies

--- main.py
+++ main.py
@@ -26,6 +26,15 @@ async def on_ready():
     print(f'Logged in as: {client.user.name} - {client.user.id}\nVersion: {discord.__version__}')
 
 
+@client.event
+async def on_message(message):
+    # Allow us to reply to webhooks and other bots
+    # please write a cooldown on this otherwise bad things will happen.
+    ctx = await client.get_context(message)
+    if ctx.valid: # Verify that the context has a command and can be used
+        await client.invoke(ctx)
+
+
 if __name__ == '__main__':
     boot_errors = []
     initial_extensions = []
